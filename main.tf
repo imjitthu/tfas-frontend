84c4a1ed0b8f3dcb1b8e3ecad0628bf4e4a8c3d9
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-19 17:50:12 +0530

    roboshop

---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "frontend" {
+  # aws_spot_instabce_request for spot instance
+  ami = "${var.AMI}"
+  instance_type = "${var.INSTANCE_TYPE}"
+  key_name = "test"
+  # spot_type = "one-time"  aws_spot_instance_request
+  tags = {
+    "Name" = "${var.COMPONENT}-Server"
+  }
+
+provisioner "local-exec" {
+  command = "ansible-playbook -i ${aws_instance.frontend.private_ip}, -u root -p ${var.PASSWORD} ${var.COMPONENT}.yml"
+}
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "${var.R53_ZONE_ID}"
+  name = "${var.COMPONENT}.${var.DOMAIN}"
+  type = "A"
+  ttl = "300"
+  records = [ aws_instance.frontend.public_ip ]
+}
+
+output "Frontend_PIP" {
+  value = aws_instance.frontend.public_ip
+}
---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "INSTANCE_TYPE" {
+    default = ""
+}
+
+variable "R53_ZONE_ID" {}
+
+variable "DOMAIN" {default = ""}
+
+variable "COMPONENT" {}
+variable "AMI" {}
+variable "PASSWORD" {}
