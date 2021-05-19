2ef420364bf64b3d7dd33062dad3636cf83912db
Jithendar Dharmapuri <jithendardharmapuri@gmail.com>
2021-05-19 19:30:19 +0530

    roboshop

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "frontend" {
 connection {
   host = aws_instance.frontend.public_ip
   type = "ssh"
-  user = "root"
+  user = "centos"
   private_key = file("${local.key_path}")
   #password = "${var.PASSWORD}"
 }
